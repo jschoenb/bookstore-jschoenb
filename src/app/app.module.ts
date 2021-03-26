bff6f88c0eb4d7a38de004f39a58203f10da8063
jschoenb <johannes.schoenboeck@fh-hagenberg.at>
2021-03-26 11:25:48 +0100

    Added Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from "./app.component";
 import { BookListComponent } from "./book-list/book-list.component";
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookStoreService } from './shared/book-store.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, BookListComponent, BookListItemComponent, BookDetailsComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [BookStoreService]
 })
 export class AppModule {}
