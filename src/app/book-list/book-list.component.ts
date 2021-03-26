358ab60c1cf2fb5d070daaf57afc64540db31b99
jschoenb <johannes.schoenboeck@fh-hagenberg.at>
2021-03-26 09:39:28 +0100

    4 files modified, 4 added

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "bs-book-list",
+  templateUrl: "./book-list.component.html"
+})
+export class BookListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
