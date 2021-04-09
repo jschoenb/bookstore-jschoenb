2fa0ecc8bdb7a954f7469fe9040a9388760344aa
jschoenb <johannes.schoenboeck@fh-hagenberg.at>
2021-04-09 21:03:27 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SearchComponent } from './search/search.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ToastrModule} from "ngx-toastr";
 
+
 @NgModule({
   imports: [BrowserModule, FormsModule,AppRoutingModule, HttpClientModule,
     BrowserAnimationsModule, // required animations module
